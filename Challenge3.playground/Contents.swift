9006ca589d5ca351f583121050d71bbef85c4260
MelissaGarrett <melissa_garrett@hotmail.com>
2021-08-26 20:41:40 -0500

    Challenge to determine if two strings contain the same characters.

---
+++ Challenge3.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func containSameCharacters(inputOne: String, inputTwo: String) -> Bool {
+    let sortedStringOne = inputOne.sorted()
+    let sortedStringTwo = inputTwo.sorted()
+    
+    return sortedStringOne == sortedStringTwo
+}
+
+assert(containSameCharacters(inputOne: "abca", inputTwo: "abca") == true, "Challenge 3 failed")
+assert(containSameCharacters(inputOne: "abc", inputTwo: "cba") == true, "Challenge 3 failed")
+assert(containSameCharacters(inputOne: "a1 b2", inputTwo: "b1 a2") == true, "Challenge 3 failed")
+assert(containSameCharacters(inputOne: "abc", inputTwo: "abca") == false, "Challenge 3 failed")
+assert(containSameCharacters(inputOne: "abc", inputTwo: "Abc") == false, "Challenge 3 failed")
+assert(containSameCharacters(inputOne: "abc", inputTwo: "cbAa") == false, "Challenge 3 failed")
+assert(containSameCharacters(inputOne: "abcc", inputTwo: "abca") == false, "Challenge 3 failed")
