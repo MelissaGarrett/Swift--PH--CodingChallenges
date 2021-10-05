46db6288793e49253d45c8287a26bc2eda5e403c
MelissaGarrett <melissa_garrett@hotmail.com>
2021-10-04 23:07:57 -0500

    Challenge to see if one string contains another.

--- Challenge4.playground/Contents.swift
+++ Challenge4.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+/* Write own version of contains() method on String
+ that ignores letter case without using the existing contains() method
+ */
+
+// Not my solution
+
+extension String {
+    func stringContainsAnother(_ string: String) -> Bool {
+        return self.lowercased().range(of: string.lowercased()) != nil
+    }
+}
+
+assert("Hello, world".stringContainsAnother("Hello") == true, "Challenge 4 failed")
+assert("Hello, world".stringContainsAnother("WORLD") == true, "Challenge 4 failed")
+assert("Hello, world".stringContainsAnother("Goodbye") == false, "Challenge 4 failed")
